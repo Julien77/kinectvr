e85a912d9547133e8cc725074673abcf21de73eb
Norbert Bus <busnorbert@gmail.com>
2011-11-22 02:39:17 +0100

    windows specific modifications for sound

--- samples/BlockKinectVR/include/Resources.h
+++ samples/BlockKinectVR/include/Resources.h
@@ -1,5 +1,12 @@
 #pragma once
+
+#if  defined(_MSC_VER) || defined(_WIN32)
+//Windows stuff
+#define RES_SOUND1			128
+
+#else
 #include "cinder/CinderResources.h"
 
 #define RES_SOUND1			CINDER_RESOURCE( ../resources/, guitar.mp3, 128, MP3 )
 //#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 129, IMAGE )
+#endif
